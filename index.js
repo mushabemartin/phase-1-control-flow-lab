6c0086866ada57acbcaf3adfeb8731cb52a9f379
MUSHABE <mushabe.martin@student.moringaschool.com>
2023-09-19 23:39:58 +0300

    javascript commit

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(rideDistance) {
+  let response;
+  if (rideDistance <= 400) {
+    response = "This one is on me!";
+  }
+  if (rideDistance > 2000) {
+    response = "I will gladly take your thirty bucks.";
+  }
+  if (rideDistance > 2500) {
+    response = "No can do.";
+  }
+  return response;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(cityName){
+  let feedback;
+  if (cityName === "NYC") {
+    feedback = "Ok, sounds good.";
+  }else {
+    feedback = "No go.";
+  }
+  return feedback;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipAmount){
+  let reply;
+  switch (tipAmount) {
+    case "generous":
+      reply = "Thank you so much.";
+      break;
+    case "not as generous":
+      reply = "Thank you.";
+      break;
+    default:
+      reply = "Bye.";
+  }
+  return reply;
 }
